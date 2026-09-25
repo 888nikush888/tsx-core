9552d89dd5760b7031a500448802381c5744f5a1
888nikush888 <89394004+888nikush888@users.noreply.github.com>
2026-09-25 19:17:42 +0200

    Bind final receipt to scoped Codacy exceptions

--- exchange_executor/ccxt_implementation_reviews.py
+++ exchange_executor/ccxt_implementation_reviews.py
@@ -7,5 +7,5 @@ commitment. An empty map grants no V2 approval; account/history data are untouch
 from types import MappingProxyType
 
 APPROVED_IMPLEMENTATION_RECEIPTS = MappingProxyType({
-    ('hyperliquid', 1): ('80abc86c5aa1d379cf39ef8ab8756b1d16ffa774e073cc52ce1273b349ff6999',),
+    ('hyperliquid', 1): ('7a3e79b9b4cc3a41070eec30a2cdacf878e15c766a204b6b4d9e6e5f4fae3c4a',),
 })
